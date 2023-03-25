ff499674adaf09940cb9b692bb72d37b87bf5093
Jp <jpsiegel8@gmail.com>
2023-03-25 18:10:15 -0400

    Bg color switch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React, { Suspense, useRef } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { Environment } from '@react-three/drei';
-import Overlay from './Overlay';
-import Model from './Model';
+import React, { Suspense, useEffect, useRef, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment } from "@react-three/drei";
+import Overlay from "./Overlay";
+import Model from "./Model";
 
 function App() {
-  const overlay = useRef()
-  const caption = useRef()
-  const scroll = useRef(0)
+  const [pyodide, setPyodide] = useState(null);
+  const [addedScript, setAddedScript] = useState(false);
+  const overlay = useRef();
+  const caption = useRef();
+  const scroll = useRef(0);
+  useEffect(() => {
+    if (!addedScript) {
+      const $pyodide = document.createElement("script");
+      $pyodide.src = "https://cdn.jsdelivr.net/pyodide/dev/full/pyodide.js";
+      $pyodide.type = "text/javascript";
+      $pyodide.onload = () => {
+        setAddedScript(true);
+      };
+      document.head.appendChild($pyodide);
+    } else if (pyodide == null) {
+      window
+        .loadPyodide({
+          indexURL: "https://cdn.jsdelivr.net/pyodide/dev/full/",
+        })
+        .then((py) => {
+          setPyodide(py);
+        });
+    }
+  }, [addedScript, setAddedScript, pyodide]);
   return (
     <>
       <Canvas
         shadows
         onCreated={(state) => state.events.connect(overlay.current)}
-        raycaster={{ computeOffsets: ({ clientX, clientY }) => ({ offsetX: clientX, offsetY: clientY }) }}>
+        raycaster={{
+          computeOffsets: ({ clientX, clientY }) => ({
+            offsetX: clientX,
+            offsetY: clientY,
+          }),
+        }}
+      >
         <ambientLight intensity={1} />
         <Suspense fallback={null}>
-          <Model scroll={scroll} />
+          <Model scroll={scroll} pyodide={pyodide} />
           <Environment preset="city" />
         </Suspense>
       </Canvas>
-      <Overlay ref={overlay} caption={caption} scroll={scroll} />
+      <Overlay
+        ref={overlay}
+        caption={caption}
+        scroll={scroll}
+        pyodide={pyodide}
+      />
     </>
   );
 }
--- src/Overlay.js
+++ src/Overlay.js
@@ -1,42 +1,65 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useState } from "react";
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import text_to_asl from "!!raw-loader!./python/text_to_asl.py";
 
-const Overlay = forwardRef(({ caption, scroll }, ref) => (
-  <div
-    ref={ref}
-    onScroll={(e) => {
-      scroll.current =
-        e.target.scrollTop / (e.target.scrollHeight - window.innerHeight);
-      caption.current.innerText = scroll.current < 0.05 ? "Scroll down" : "";
-    }}
-    className="scroll"
-  >
-    <div class="logo">
-      <img src="placeholder_logo.png" alt="" height="300" width="300" />
+const SCROLL_THRESHOLD = 0.5;
+
+const Overlay = forwardRef(({ caption, scroll, pyodide }, ref) => {
+  const [backgroundColor, setBackgroundColor] = useState("white");
+
+  return (
+    <div
+      ref={ref}
+      onScroll={(e) => {
+        scroll.current =
+          e.target.scrollTop / (e.target.scrollHeight - window.innerHeight);
+        caption.current.innerText = scroll.current < 0.05 ? "Scroll down" : "";
+        if (scroll.current > SCROLL_THRESHOLD) {
+          setBackgroundColor("black");
+        }
+      }}
+      className="scroll"
+    >
+      <button
+        onClick={async (event) => {
+          if (pyodide != null) {
+            console.log(text_to_asl);
+            console.log(pyodide.runPython(text_to_asl));
+          } else {
+            console.log("Pyodide not loaded yet");
+          }
+        }}
+      >
+        Hello
+      </button>
+      <div class="logo">
+        <img src="placeholder_logo.png" alt="" height="300" width="300" />
+      </div>
+      <div class="container">
+        <div class="item">
+          <div class="icon_images">
+            <img id="t2asl_image" alt="" src="T2ASL_Placeholder.png" />
+          </div>
+          <div class="box_title">Text to ASL</div>
+        </div>
+        <div class="item">
+          <div class="icon_images">
+            <img id="s2t_image" alt="" src="Speech2ASL_Placehoder.png" />
+          </div>
+          <div class="box_title">Text to ASL</div>
+        </div>
+        <div class="item">
+          <div class="icon_images">
+            <img id="live_image" alt="" src="LiveSTT_Placehoder.jpg" />
+          </div>
+          <div class="box_title">Text to ASL</div>
+        </div>
+      </div>
+      <span className="caption" ref={caption}>
+        Scroll down
+      </span>
     </div>
-    <div class="container">
-      <div class="item">
-        <div class="icon_images">
-          <img id="t2asl_image" alt="" src="T2ASL_Placeholder.png" />
-        </div>
-        <div class="box_title">Text to ASL</div>
-      </div>
-      <div class="item">
-        <div class="icon_images">
-          <img id="s2t_image" alt="" src="Speech2ASL_Placehoder.png" />
-        </div>
-        <div class="box_title">Text to ASL</div>
-      </div>
-      <div class="item">
-        <div class="icon_images">
-          <img id="live_image" alt="" src="LiveSTT_Placehoder.jpg" />
-        </div>
-        <div class="box_title">Text to ASL</div>
-      </div>
-    </div>
-    <span className="caption" ref={caption}>
-      Scroll down
-    </span>
-  </div>
-));
+  );
+});
 
 export default Overlay;
